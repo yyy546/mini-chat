b4b20e7344de8fe573606f1ef555786ba1949cac
czy <1330349611@qq.com>
2026-05-22 17:18:45 +0800

    refactor:Element Plus 按需导入

--- minichat-frontend/src/main.ts
+++ minichat-frontend/src/main.ts
@@ -1,7 +1,5 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import App from './App.vue'
 import router from './router'
@@ -13,7 +11,6 @@ const pinia = createPinia()
 
 app.use(pinia)
 app.use(router)
-app.use(ElementPlus)
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
